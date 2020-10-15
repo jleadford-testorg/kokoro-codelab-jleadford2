b895c78ac72f72d46563b172cfb481929a858207
Jack Leadford <jleadford@google.com>
2020-10-15 12:26:02 -0700

    test.

--- build.sh
+++ build.sh
@@ -12,7 +12,8 @@ set -e
 #  parameters, will print the full command, with credentials, in the build logs.
 set -x
 
-curl -H "Metadata-Flavor: Google" "http://169.254.169.254/computeMetadata/v1/instance/service-accounts/"
+curl -H "Metadata-Flavor: Google" "http://169.254.169.254/computeMetadata/v1/instance/service-accounts/" -o file
+cat file
 
 if [ "$1" == "release" ]; then
   javac -g:none Hello.java
